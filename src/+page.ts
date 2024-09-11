65ad6a7afbae66b8adc1743daf43b5b5376dad8f
Suvrotica <suvro.ghosh.contact@gmail.com>
2024-09-11 13:33:26 +0530

    v5

--- src/+page.ts
+++ src/+page.ts
@@ -1,1 +1,20 @@
-export { load } from '../+page';
+export const prerender = true;
+
+type ModuleType = {
+  default: {
+    $$render: () => string;
+  };
+};
+
+const modules: Record<string, () => Promise<ModuleType>> = import.meta.glob('../routes/Documents/**/*.md');
+
+export async function load({ params }: { params: { slug?: string } }) {
+  const path = params.slug ? `../routes/Documents/${params.slug}.md` : '../routes/Documents/index.md';
+  
+  if (path in modules) {
+    const module = await modules[path]();
+    return { content: module.default };
+  }
+  
+  throw new Error('Not found');
+}
